4fa6f095d406296c8138cdb1bcf76e16eaa07ef8
Adrian <164921227+silvaadrian@users.noreply.github.com>
2025-09-10 08:14:06 -0300

    update: swagger.js - Adicionando API_URL

--- back-end/swagger.js
+++ back-end/swagger.js
@@ -19,7 +19,7 @@ const options = {
     },
     servers: [
       {
-        url: 'http://localhost:5000/api',
+        url: process.env.API_URL || 'http://localhost:5000/api',
       },
     ],
   },
